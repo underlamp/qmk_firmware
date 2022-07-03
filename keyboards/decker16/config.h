24c5f8544d48d4bd6ddc29586e1327c5a30c2b69
siraphobk <siraphob.k@outlook.com>
2022-07-03 23:22:00 +0700

    configure first keymap

--- keyboards/decker16/config.h
+++ keyboards/decker16/config.h
@@ -19,5 +19,21 @@
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
 
-#define VENDOR_ID 0x1234
+//
+//
+
+/* Metatdata */
+#define VENDOR_ID 0xFEED
 #define PRODUCT_ID 0x5678
+
+/* Physical configuration */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 4
+
+// Pin definition can be studied at qmk_firmware/platforms/chibios/_pin_defs.h
+#define MATRIX_ROW_PINS \
+    { D1, D3, D5, D7 }
+#define MATRIX_COL_PINS \
+    { D2, D4, D6, B3 }
+
+#define DIODE_DIRECTION COL2ROW // COL2ROW := Black mark of the diode is facing to the rows.
---
+++ keyboards/decker16/keymaps/default/keymap.h
@@ -0,0 +1,13 @@
+#include "quantum.h"
+
+#define LAYOUT( \
+    KC_P7, KC_P8,   KC_P9,   KC_NO,   \
+    KC_P4, KC_P5,   KC_P6,   KC_PAST, \
+    KC_P1, KC_P2,   KC_P3,   KC_PPLS, \
+    KC_P0, KC_PEQL, KC_PDOT, KC_PMNS  \
+) { \
+    { KC_P7, KC_P8,   KC_P9,   KC_NO   }, \
+    { KC_P4, KC_P5,   KC_P6,   KC_PAST }, \
+    { KC_P1, KC_P2,   KC_P3,   KC_PPLS }, \
+    { KC_P0, KC_PEQL, KC_PDOT, KC_PMNS }  \
+}
