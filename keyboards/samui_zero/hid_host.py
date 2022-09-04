212af98fc528eab07a3f0aa8c32fc278f8eca817
siraphob-api <siraphob@apiplstech.com>
2022-09-05 00:35:27 +0700

    implement keyboard class

--- keyboards/samui_zero/hid_host.py
+++ keyboards/samui_zero/hid_host.py
@@ -1,7 +1,5 @@
 import hid
 
-EP_SIZE: int = 32
-
 
 def pad_message(payload: bytes) -> bytes:
     if len(payload) > EP_SIZE:
@@ -9,36 +7,57 @@ def pad_message(payload: bytes) -> bytes:
     return payload + b'\x00' * (EP_SIZE - len(payload))
 
 
-def to_bytes(data: str) -> bytes:
-    return data.encode()
+class Keyboard:
+    def __init__(self, ep_size: int, vendor_id: int, product_id: int, usage_page: int, usage_id: int) -> None:
+        self.device = None
+        self.ep_size = ep_size
+
+        for device in hid.enumerate():
+            if device['vendor_id'] == vendor_id and device['product_id'] == product_id \
+                and device['usage_page'] == usage_page and device['usage'] == usage_id:
+                self.device = hid.Device(path=device['path'])
+
+        if self.device == None:
+            print("error: could not find the keyboard")
+            exit(1)
+
+    def print_device_info(self):
+        print("== Keyboard information ==")
+        print("Manufacturer:", self.device.manufacturer)
+        print("Product:", self.device.product)
+
+    def send(self, payload: bytes):
+        self.device.write(pad_message(payload))
+
+    def read(self) -> bytes:
+        return self.device.read(self.ep_size)
+
+    def transceive(self, payload: bytes) -> bytes:
+        """Send and read information from the device.
+
+        Args:
+            payload (bytes): The message to send to the device. The message must not be larger than `self.ep_size`.
+
+        Returns:
+            bytes: The message received from the device.
+        """
+        self.send(payload)
+        return self.read(self.ep_size)
+
+    def close(self):
+        self.device.close()
 
 
+EP_SIZE = 32
 vid = 0xFEED
 pid = 0xC0B2
 usage_page = 0xff60
 usage_id = 0x61
 
-keyboard: hid.Device = None
-
-for device in hid.enumerate():
-    if device['vendor_id'] == vid and device['product_id'] == pid \
-        and device['usage_page'] == usage_page and device['usage'] == usage_id:
-        keyboard = hid.Device(path=device['path'])
-
-if keyboard == None:
-    print("Keyboard was not found.")
-    exit(1)
-
-print("Keyboard found!")
-print("Product:", keyboard.product)
-print("Manufacturer:", keyboard.manufacturer)
-
-message = pad_message(b'hello world')
-print("sending:", message)
-keyboard.write(message)
-print("reading...")
-received = keyboard.read(EP_SIZE)
-print("received:", received)
-
+keyboard = Keyboard(ep_size=EP_SIZE,\
+                    vendor_id=vid,\
+                    product_id=pid,\
+                    usage_page=usage_page,\
+                    usage_id=usage_id)
+keyboard.print_device_info()
 keyboard.close()
-print("device closed")
